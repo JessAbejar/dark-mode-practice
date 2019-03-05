b7d21b7155dbe44bbc50186010b4f2b58f4b0e14
jgab1297 <jessica.abejar@gmail.com>
2019-03-05 17:03:51 -0500

    Styling

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-var body = document.querySelector(".body");
+// Variables
+var body = document.querySelector("body");
 var btn = document.querySelector(".btn");
 var btnText = document.getElementById("btn-text")
 
+// Initial Button Text
 btnText.innerHTML = "Dark-Mode";
 
-function testFunc(){
-    console.log("clicked!")
-
+function modeSwitch(){
+    // Light-Mode to Dark-Mode
     if (btn.classList.contains('on') == true) {
         btn.classList.remove('on');
         btn.classList.add('off');
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
         btnText.innerHTML = "Light-Mode";
-    } else if (btn.classList.contains('off') == true) {
+    } 
+    // Dark-Mode to Light-Mode
+    else if (btn.classList.contains('off') == true) {
         btn.classList.remove('off');
         btn.classList.add('on');
         body.classList.remove('dark-mode');
