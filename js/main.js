498019ca4532b17ce88a335a4d7d747acd4fcd9e
jgab1297 <jessica.abejar@gmail.com>
2019-03-07 20:50:54 -0500

    JS refactored

--- js/main.js
+++ js/main.js
@@ -1,29 +1,12 @@
 // Variables
-var body = document.querySelector("body");
-var btn = document.querySelector(".btn");
-var btnText = document.getElementById("btn-text")
-
-// Initial Button Text
-btnText.innerHTML = '<i class="fas fa-moon">&nbsp;</i>Dark-Mode';
+var body = document.querySelector('body');
+var btns = document.querySelectorAll('.btn');
 
 function modeSwitch(){
-    // Light-Mode to Dark-Mode
-    if (btn.classList.contains('on') == true) {
-        btn.classList.remove('on');
-        btn.classList.add('off');
-        body.classList.remove('light-mode');
-        body.classList.add('dark-mode');
-        btnText.innerHTML = '<i class="fas fa-sun">&nbsp;</i>Light-Mode';
-    } 
-    // Dark-Mode to Light-Mode
-    else if (btn.classList.contains('off') == true) {
-        btn.classList.remove('off');
-        btn.classList.add('on');
-        body.classList.remove('dark-mode');
-        body.classList.add('light-mode');
-        btnText.innerHTML = '<i class="fas fa-moon">&nbsp;</i>Dark-Mode';
-    }
+    body.classList.toggle('dark-mode');
 
 }
 
-btn.addEventListener('click', modeSwitch)
+for (let btn of btns) {
+    btn.addEventListener('click', modeSwitch)
+}
