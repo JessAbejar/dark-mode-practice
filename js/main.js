203f4e5019cca5beb4c09ab420a98722816ca6c7
jgab1297 <jessica.abejar@gmail.com>
2019-03-07 19:47:56 -0500

    Add Event Listener

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var btn = document.querySelector(".btn");
 var btnText = document.getElementById("btn-text")
 
 // Initial Button Text
-btnText.innerHTML = "Dark-Mode";
+btnText.innerHTML = '<i class="fas fa-moon">&nbsp;</i>Dark-Mode';
 
 function modeSwitch(){
     // Light-Mode to Dark-Mode
@@ -13,7 +13,7 @@ function modeSwitch(){
         btn.classList.add('off');
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
-        btnText.innerHTML = "Light-Mode";
+        btnText.innerHTML = '<i class="fas fa-sun">&nbsp;</i>Light-Mode';
     } 
     // Dark-Mode to Light-Mode
     else if (btn.classList.contains('off') == true) {
@@ -21,7 +21,9 @@ function modeSwitch(){
         btn.classList.add('on');
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
-        btnText.innerHTML = "Dark-Mode";
+        btnText.innerHTML = '<i class="fas fa-moon">&nbsp;</i>Dark-Mode';
     }
 
 }
+
+btn.addEventListener('click', modeSwitch)
