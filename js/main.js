6f788a90c7c34a1d5d865f3c16ed523ad45df24f
jgab1297 <jessica.abejar@gmail.com>
2019-04-22 20:58:49 -0400

    IE9 Compatibility for JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 var body = document.getElementById('body');
 var btn = document.getElementById('color-theme-btn');
 
-function colorThemeSwitch(){
-    body.classList.toggle('dark-mode');
+var colorThemeSwitch = function() {
+    if (body.classList) {
+        body.classList.toggle('dark-mode');
+    } else {
+        var bodyClasses = body.className.split(' ');
+        var darkModeClassIndex = bodyClasses.indexOf('dark-mode');
+
+        if (darkModeClassIndex >= 0) {
+            bodyClasses.splice(darkModeClassIndex, 1);
+        } else {
+            bodyClasses.push('dark-mode');
+            body.className = bodyClasses.join(' ')
+        }
+    }
 }
 
 btn.addEventListener('click', colorThemeSwitch)
